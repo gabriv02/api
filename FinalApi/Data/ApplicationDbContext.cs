8936fac7ade810e27c433504d87435f04680566b
Gabriela <a2020120887@estudiantes.upsa.edu.bo>
2021-12-08 19:44:48 -0400

    first commit

---
+++ FinalApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FinalApi.Auth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalApi.Models;
+
+namespace FinalApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<FinalApi.Models.Person> Person { get; set; }
+    }
+}
